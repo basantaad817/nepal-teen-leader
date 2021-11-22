3350361ced2cf20f952d3390a3dea6babc20e528
Basan <basantaad817@gmail.com>
2021-11-22 12:21:02 +0545

    navigation bar responsive

---
+++ script.js
@@ -0,0 +1,11 @@
+// menu bar
+
+let menu = document.querySelector('#menu-bar');
+let navbar = document.querySelector('.navbar');
+
+menu.onclick = () =>{
+
+  menu.classList.toggle('fa-times');
+  navbar.classList.toggle('active');
+
+}
